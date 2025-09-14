755451a5a9c3945ee17a44b365841cfa2ec4fd5c
SUPRIYAKUKUNURU <supriyakukunuru2001@gmail.com>
2025-09-14 11:08:54 +0530

    Add shallow.js with shallow copy example

---
+++ Shallow.js
@@ -0,0 +1,12 @@
+let original ={
+    Name:"Supriya",
+    address:{city:"Hyderbad"},
+}
+let shallowCopy = Object.assign({},original)
+// let shallowCopy = {...original};
+original.address.city="Bagalore";
+console.log(shallowCopy.address.city);
+
+// The addrees whenever we changed in original automatically address changed in shallowCopy
+// this is because their memory reference is same .Shallow effects the parent node
+// output changed
---
+++ deepCopy.js
@@ -0,0 +1,11 @@
+let original ={
+    name :"Supriya",
+    address:{city:"Hyderabad"}
+};
+
+let deepCopy=JSON.parse(JSON.stringify(original));
+original.address.city="Bangalore";
+console.log(deepCopy.address.city);
+
+// we have changed the original but the output is not changed this is 
+// beacause their memory reference is different
